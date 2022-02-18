3e6fb8cb10f0445a0083c06449c3bf077a391456
Frederic_Bui <bui.frederic.pro@gmail.com>
2022-02-18 16:26:05 +0000

    subject and object to form

--- index.php
+++ index.php
@@ -3,9 +3,20 @@
     <title>PHP Test</title>
   </head>
   <body>
+    <!--Form send email-->
     <form action="mailSuccess.php">
-      <label>Email</label>
-      <input type="email" name="email"></input>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email"></input>
+      </div>
+      <div>
+        <label>Subject</label>
+        <input type="text" name="subject"></input>
+      </div>
+      <div>
+        <label>Object</label>
+        <input type="text" name="object"></input>
+      </div>
       <button type="submit">Send</button>
     </form>
 
